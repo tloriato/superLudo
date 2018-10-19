22c64a95e45dc4d97613548fcfe66ca118499516
Tiago Loriato <tloriatosimoes@gmail.com>
2018-10-19 18:31:47 -0300

    Hello World

---
+++ src/PrimFrame.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class PrimFrame extends JFrame {
+	/**
+	 * Hello World of GUI 
+	 */
+	
+	private static final long serialVersionUID = 1L;
+	public final int LARG_DEFAULT = 960;
+	public final int ALT_DEFAULT = 720;
+	
+	public PrimFrame() {
+		setSize(LARG_DEFAULT, ALT_DEFAULT);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+}
---
+++ src/SuperLudo.java
@@ -0,0 +1,10 @@
+
+public class SuperLudo {
+
+	public static void main(String[] args) {		
+		PrimFrame f = new PrimFrame();
+		f.setTitle("Minha primeira GUI");
+		f.setVisible(true);
+	}
+
+}
