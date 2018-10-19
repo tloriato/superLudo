9531b3ea6a1ab1f55209f5cf95593788303067e3
Tiago Loriato <tloriatosimoes@gmail.com>
2018-10-19 20:15:11 -0300

    versão 0.000001

--- src/PrimFrame.java
+++ src/PrimFrame.java
@@ -17,9 +17,29 @@ public class PrimFrame extends JFrame {
 		int x=screenSize.width/2-LARG_DEFAULT/2;
 		int y=screenSize.height/2-ALT_DEFAULT/2;
 		
-		setBounds(x,y,LARG_DEFAULT,ALT_DEFAULT);
-		setResizable(false);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setBounds(x,y,LARG_DEFAULT,ALT_DEFAULT);
+		this.setResizable(false);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		/* Menu Lateral */
+		Panel pRight = new Panel(new GridLayout(6,1));
+		pRight.setBackground(Color.gray);
+		
+		JButton newGame = new JButton("Novo Jogo");
+		JButton loadGame = new JButton("Carregar Jogo");
+		JButton saveGame = new JButton("Salvar");
+		Label currentlyPlaying = new Label("À Jogar:");
+		JButton throwDice = new JButton("Lançar Dado");
+		
+		pRight.add(newGame);
+		pRight.add(loadGame);
+		pRight.add(saveGame);
+		pRight.add(currentlyPlaying);
+		pRight.add(throwDice);		
+		/*Fim do Menu Lateral */
+		
+		add(pRight, BorderLayout.EAST);
+		
 	}
 
 }
